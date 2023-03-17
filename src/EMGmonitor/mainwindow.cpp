b60ea78dd78e173d0f87c95101591673354d3af7
Midhun <midhunjac2423@gmail.com>
2023-03-18 04:34:34 +0530

    Committing files for mainwindow
    First files for the mainwindow

--- src/EMGmonitor/mainwindow.cpp
+++ src/EMGmonitor/mainwindow.cpp
@@ -1,15 +1,76 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QPixmap>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent){
+
+    init_UI();
+
+
 }
-
 MainWindow::~MainWindow()
 {
-    delete ui;
+    //delete ui;
+}
+
+void MainWindow::init_UI()
+{
+
+    QFont title_font("Arial", 20, QFont::Bold);
+    QFont slogan_font("Arial",10,10,true);
+
+    title_text = new QLabel("Physionix", this);
+    title_text->setFont(title_font);
+    slogan_text = new QLabel("Your physio",this);
+    slogan_text->setFont(slogan_font);
+
+    but_viewdemo = new QPushButton("Watch", this);
+    but_monitor = new QPushButton("Start", this);
+
+    txt_viewdemo = new QLabel("View tutorial video", this);
+    txt_monitor = new QLabel("Start monitoring", this);
+
+
+
+     QGridLayout *title_layout = new QGridLayout;
+     title_layout->addWidget(title_text,0,0,Qt::AlignCenter);
+     title_layout->addWidget(slogan_text,1,0,Qt::AlignCenter);
+
+
+    QGridLayout *button_layout = new QGridLayout;
+    button_layout->setHorizontalSpacing(10);
+    button_layout->setVerticalSpacing(100);
+    button_layout->addWidget(but_viewdemo, 0, 0, Qt::AlignLeft);
+    button_layout->addWidget(txt_viewdemo, 0, 1);
+    button_layout->addWidget(but_monitor, 1, 0, Qt::AlignLeft);
+    button_layout->addWidget(txt_monitor, 1, 1);
+
+
+
+    QHBoxLayout *grid_layout = new QHBoxLayout;
+    grid_layout->addStretch(1);
+    grid_layout->addLayout(button_layout);
+    grid_layout->addStretch(1);
+
+
+
+
+
+    QVBoxLayout *main_window_layout = new QVBoxLayout;
+    main_window_layout->setSpacing(10);
+
+    main_window_layout->addLayout(title_layout, Qt::AlignCenter);
+    main_window_layout->addLayout(grid_layout, Qt::AlignCenter);
+
+
+
+    QWidget *window = new QWidget();
+    window->setLayout(main_window_layout);
+
+    setCentralWidget(window);
+
+
 }
 
--- src/EMGmonitor/mainwindow.h
+++ src/EMGmonitor/mainwindow.h
@@ -1,21 +1,46 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLayout>
+#include <QBoxLayout>
+#include <QLabel>
+#include <QPixmap>
+#include <QToolButton>
+#include <QMessageBox>
+#include <QFont>
+
+#include <iostream>
+
+using namespace std;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    public:
+        explicit MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    private:
+
+        QFont title_font,slogan_font;
+        QLabel *title_text, *txt_viewdemo, *txt_monitor, *slogan_text;
+        QPushButton *but_viewdemo, *but_monitor;
+
+
+        void init_UI();
+
+
+    private slots:
+        // void timerEvent(); //depreciated: used to plot sinewave to curves
+
+
+
+
+
 };
+
 #endif // MAINWINDOW_H
